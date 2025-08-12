6c5828240981b6e11c1538867111a6abd116fb93
Nagesh <nageshballurkar2003@gmail.com>
2025-08-12 16:46:24 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+package myFirstProject;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is my repo where i can create on the pc and push into the GitHub");
+    }
+    
+    
+}
